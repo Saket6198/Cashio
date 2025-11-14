e05f039f9d2122cec32250fd28172ccb3b2cd452
Saket Singh <137860573+Saket6198@users.noreply.github.com>
2025-11-15 00:52:45 +0530

    hosted url

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const BASE_URL =
-  "https://rover-albuquerque-farms-ruling.trycloudflare.com";
+export const BASE_URL = "https://cashio-service.vercel.app";
+// "https://rover-albuquerque-farms-ruling.trycloudflare.com";

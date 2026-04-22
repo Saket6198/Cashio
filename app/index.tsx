20afc3f1233ee4929d992495e0f61940a65a4319
saket6198 <saket6198@gmail.com>
2026-04-22 10:19:52 +0530

    changed index.tsx uppercase to lowercase

---
+++ app/index.tsx
@@ -0,0 +1,37 @@
+import { useRouter } from "expo-router";
+import React, { useEffect } from "react";
+import { View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+
+const index = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/(main)/home");
+    }, 1300);
+  }, []);
+
+  return (
+    <SafeAreaProvider className="flex">
+      <View className="flex flex-1 justify-center items-center bg-neutral-900">
+        <Animated.Image
+          style={{ aspectRatio: 1, height: hp("30%") }}
+          entering={FadeInDown.delay(300).duration(800).springify()}
+          source={require("@/assets/images/Wallet1.png")}
+        />
+        <Animated.Text
+          className="text-white font-bold mt-6"
+          style={{ fontSize: hp("4%") }}
+          entering={FadeInDown.delay(300).duration(800).springify()}
+        >
+          Cash-Co
+        </Animated.Text>
+      </View>
+    </SafeAreaProvider>
+  );
+};
+
+export default index;
